07359c40b4aeaecaaa2634de9e50559d4695302f
Almir Kopic <almirkopic1992@gmail.com>
2025-03-15 21:39:16 +0100

    auth user

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
-const dataRoutes = require("./routes/api/userData");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const dataRoutes = require("./routes/api/userData");
 const registerRoute = require("./routes/userRegister");
+const loginUser = require("./routes/userAuth");
 
 const PORT = process.env.URI || 3000;
 
@@ -13,6 +15,7 @@ app.use(express.json());
 
 app.use(dataRoutes);
 app.use("/register", registerRoute);
+app.use("/auth", loginUser);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
