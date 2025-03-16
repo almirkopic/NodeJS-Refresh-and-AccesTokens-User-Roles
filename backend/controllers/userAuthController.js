7d4304085660159e36948a48a1534946d358043e
Almir Kopic <almirkopic1992@gmail.com>
2025-03-16 23:43:14 +0100

    jwt token auth

--- backend/controllers/userAuthController.js
+++ backend/controllers/userAuthController.js
@@ -6,6 +6,11 @@ const usersDB = {
 };
 
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const fsPromises = require("fs").promises;
+const path = require("path");
+const dataPath = path.join(__dirname, "..", "model", "users.json");
 
 const loginUser = async (req, res) => {
   const { user, pw } = req.body;
@@ -23,7 +28,35 @@ const loginUser = async (req, res) => {
   const match = await bcrypt.compare(pw, userFound.password);
 
   if (match) {
-    return res.status(201).json({ message: "Successfully authorized." });
+    //jwt
+    const accessToken = jwt.sign(
+      { username: userFound.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "5min" }
+    );
+
+    const refreshToken = jwt.sign(
+      { username: userFound.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    const otherUsers = usersDB.users.filter(
+      (client) => client.username !== userFound.username
+    );
+    const currentUser = { ...userFound, refreshToken };
+    usersDB.setUsers([...otherUsers, currentUser]);
+
+    await fsPromises.writeFile(dataPath, JSON.stringify(usersDB.users));
+
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.json({ accessToken });
   } else {
     return res.status(401).json({ message: "Authorization failed." });
   }
